c8e5b8c467abee9954ed7e7df2f8ad3c09d05915
rdm100 <robertode_marco@hotmail.com>
2021-01-12 23:54:48 +0000

    added sign-in-and-sign-up page and sign-in and custom-button components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.jsx';
 import Shop from './pages/shop/shop.jsx';
 import Header from './components/header/header.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />    
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.jsx';
+import './sign-in-and-sign-up.scss';
+
+const SignInAndSignUp = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUp;
